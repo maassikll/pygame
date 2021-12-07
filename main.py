51ed1a45a3fc03bfe9941100bfe5b39c6a1a3696
maassikll <94725508+maassikll@users.noreply.github.com>
2021-12-07 20:04:58 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+class  playy :
+    def __init__(self):
+        self.pseudo
+        self.health
+        self.age
+
+       player = playy("mazzi",200,20)
+    print("welcome ", player.pseudo, "point de vie", player.health, " votre age", player.age, " ans")
